74fbceb2db90a822a01b0f4423c99fa542106e7f
Arch-Justin <12guilhrm12@gmail.com>
2026-05-10 18:48:20 -0300

    Added Projects and Stuff

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -7,6 +7,69 @@ $(document).ready(function() {
     });
 });
 
+// Button functions
+
+// About Us (Landing Page)
 function btn_AboutUs() {
   window.location.href = '/aboutus';
 }
+
+// Games (Projects)
+const projects_selector = document.getElementById("projects-selector");
+const game_page = document.getElementById("games-list");
+const comic_page = document.getElementById("comics-list");
+
+function show_projects(project) {
+    projects_selector.style.display = "none"; 
+
+    // Show Project Section
+    if (project === "games") {
+        game_page.style.display = "flex";
+    }
+    else
+    if (project === "comics") {
+        comic_page.style.display = "flex";
+    }
+
+    const url = new URL(window.location);
+    url.searchParams.set("project", project);
+    window.history.replaceState({}, "", url);
+
+}
+
+function projects_reset() {
+
+    projects_selector.style.display = "flex";
+
+    game_page.style.display = "none";
+    comic_page.style.display = "none";
+
+    const url = new URL(window.location);
+    url.searchParams.delete("project");
+    window.history.replaceState({}, "", url);
+}
+
+function link_goto(_url) {
+    window.open(_url, "_blank");
+}
+
+/* abrir automaticamente */
+window.addEventListener("load", () => {
+
+  const params = new URLSearchParams(window.location.search);
+
+  const project = params.get("project");
+
+  if (project === "games") {
+    show_projects("games");
+  }
+
+  else if (project === "comics") {
+    show_projects("comics");
+  }
+
+  else {
+    projects_reset();
+  }
+
+});
